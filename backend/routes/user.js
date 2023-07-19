d759852a4d1d64e3878f5e248f0085dc1e92e22c
Rohit Mahajan <rohit.mahajan@nomissolutions.com>
2023-07-19 12:09:18 +0530

    User and Post apis built!

---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteUser, getAllUser, getFriends, getPendingFriends, getUser, signIn, signUp, updateUser } from "../controller/user";
+
+const userRouter = express.Router()
+
+userRouter.get("/getallusers", getAllUser)
+userRouter.get("/getuser/:userid", getUser)
+userRouter.post("/signup", signUp)
+userRouter.post("/signin", signIn)
+userRouter.patch("/updateuser/:userid", updateUser)
+userRouter.delete("/deleteuser/:userid", deleteUser)
+userRouter.get("/getfriends/:userid", getFriends)
+userRouter.get("/getpendingfriends/:userid", getPendingFriends)
+
+export default userRouter;
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user";
+import postRouter from "./routes/post";
+
+const app = express();
+app.use(express.json())
+app.use(userRouter)
+app.use(postRouter)
+
+mongoose.connect("mongodb+srv://rohitmforcontact:peX5huR5QfiZNlXr@rconnect.unodncx.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => app.listen(3000))   
+    .then(() => console.log("Connected!"))  
+    .catch(err => console.log(err))
